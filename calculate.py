4ab1f113930f470376b80e790d3a6cbb0492a64d
E-Sakhno <miss.saxno@yandex.ru>
2020-10-28 01:53:49 +0300

    Add try/except construction for the second value equals zero

--- calculate.py
+++ calculate.py
@@ -3,6 +3,9 @@ y=int(input("Input the second value: "))
 addit=x+y
 subst=x-y
 multi=x*y
-divis=x/y
+try:
+    divis=x/y
+except:
+    divis='cannot be divided by zero'
 print(addit, subst)
 print(multi, divis)
